71baf8de6cf1fae88383b4d07b8772d5b41853ad
Poorav <118240170+poorav03@users.noreply.github.com>
2023-10-08 08:47:20 +0530

    Add files via upload

---
+++ Lecture3-II.cpp
@@ -0,0 +1,16 @@
+//Lecture 3 II - Variables, Constant and Data Types Part - 2
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=3, b=5;
+
+    cout << a+b << endl << "a+b";
+    
+    cout << endl;
+    cout << a+b;
+    cout << endl;
+    cout << "a+b";
+    
+    return 0;
+}
